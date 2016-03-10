c3a12e01ed80ea64f8ac603cde7714a7a9ac4832
klimychevmn <plagonist@gmail.com>
2016-03-10 06:55:04 +0300

    fix about git account + add footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,15 @@
 
+<style>
+    .footer {
+        text-align: right;
+        margin-top: 20px;
+        margin-right: 20px;
+    }
+</style>
 </div><!-- /.container -->
-
-
+<footer>
+    <hr />
+    <div class="footer">2016 &copy; MyBlog</div>
+</footer>
 </body>
 </html>
